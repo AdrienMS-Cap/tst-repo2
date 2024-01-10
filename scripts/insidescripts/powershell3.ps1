ef9a45ac643436603b633ea99d8f92ce0248da23
Adrien MENDES SANTOS <adrien.mendes-santos@capgemini.com>
2024-01-10 08:52:27 +0100

    rr

--- scripts/insidescripts/powershell3.ps1
+++ scripts/insidescripts/powershell3.ps1
@@ -1,3 +1,15 @@
-# This is a "Hello, World!" script that will fail.
-Write-Host "Hello, World!"
-throw "Error: This is a forced error."
+# Incorrect variable naming
+$incorrectVariable = "Hello"
+
+# Incorrect indentation
+  Write-Host "This is incorrectly indented"
+
+# Missing mandatory parameter
+function Test-Function {
+    param (
+        [Parameter()]
+        $MissingParameter
+    )
+
+    Write-Host "Function executed"
+}
