73b65506ec1192b9fb6ba21bccac7e2433dfa4eb
Adrien MENDES SANTOS <adrien.mendes-santos@capgemini.com>
2024-01-10 09:03:14 +0100

    b

--- scripts/insidescripts/powershell3.ps1
+++ scripts/insidescripts/powershell3.ps1
@@ -3,3 +3,6 @@ Write-Host "This is a deliberate issue"
 $undefinedVariable = $undefinedVariable + 1
 
 ConvertTo-SecureString "fake" -AsPlainText -Force
+
+# Intentionally cause a failure
+$variableDoesNotExist
